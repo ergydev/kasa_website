19cc77138369f94416acda084b19671b49b77f2c
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-06-06 17:49:30 +0200

    added footer css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,12 @@ import './footer.scss'
 
 function Footer() {
     return(
-        <section id='footer'>
+        <footer>
             <div className="footer__center">
                 <Link to='/'><img src={logoFooter} alt="Accueil" className='footer__center--logo' /></Link>
                 <p className='footer__center--copyright'>&copy; 2020 Kasa. All rigths reserved</p>
             </div>
-        </section>
+        </footer>
     )
 }
 export default Footer
